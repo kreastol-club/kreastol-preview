b0261318f7a0af50835a9d4198033e0c36d7b049
Joshua Hegedus <jhegedus9@gmail.com>
2023-04-03 15:17:32 +0200

    fixed init

--- kreastol.v2/app/controllers/application_controller.rb
+++ kreastol.v2/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :init
+  # before_action :init
 
   # helper_method :hex_to_rgb, :rgb_to_hsv, :hsv_to_rgb, :rgb_to_hex, :get_complementary
 end
